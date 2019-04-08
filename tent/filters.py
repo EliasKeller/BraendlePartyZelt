348f4b5da4a13fd206c9d47a477da4c6e9e6649c
Elias Keller <elias.keller@gmail.com>
2019-04-08 11:38:21 +0200

    feat: added number_of_people_range filter it works

--- tent/filters.py
+++ tent/filters.py
@@ -1,8 +1,14 @@
 from tent.models import Tent
+from django_filters import RangeFilter
 import django_filters
 
 
 class TentFilter(django_filters.FilterSet):
+    number_of_People = RangeFilter(label="People range")
+
     class Meta:
         model = Tent
-        fields = ['width', 'length', ]
+        fields = ['width', 'length', 'number_of_People', ]
+
+
+
