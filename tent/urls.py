9df32019f6368d3a89ec38078b2bd40e138b7617
Elias Keller <elias.kelle@gmail.com>
2021-02-04 00:01:56 +0100

    feat: whole styling

--- tent/urls.py
+++ tent/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('partyzelte/', views.party_tent_search, name='party_tent_search'),
     path('equipment/', views.equipment, name='equipment'),
-    path('offer/', views.offer, name='offer')
+    path('offer/', views.offer, name='offer'),
+    path('aboutMe/', views.aboutMe, name='about_me')
 
 ]
